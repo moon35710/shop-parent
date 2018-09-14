c6199db37c5c385885bfeb08632401b6310e6ba1
moon35710 <w819210252>
2018-09-14 17:51:17 +0800

    add

---
+++ shop.sql
@@ -0,0 +1,207 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : hadooptest.mysql.rds.aliyuncs.com
+Source Server Version : 50718
+Source Host           : hadooptest.mysql.rds.aliyuncs.com:3316
+Source Database       : shop
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2018-09-14 17:50:32
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `goods_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品id(SKU)',
+  `goods_name` varchar(100) NOT NULL DEFAULT '' COMMENT '商品名称',
+  `shop_id` int(10) unsigned NOT NULL DEFAULT '1' COMMENT '店铺id',
+  `category_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '商品分类id',
+  `category_id_1` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '一级分类id',
+  `category_id_2` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '二级分类id',
+  `category_id_3` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '三级分类id',
+  `brand_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '品牌id',
+  `group_id_array` varchar(255) NOT NULL DEFAULT '' COMMENT '店铺分类id 首尾用,隔开',
+  `promotion_type` tinyint(3) NOT NULL DEFAULT '0' COMMENT '促销类型 0无促销，1团购，2限时折扣',
+  `promote_id` int(11) NOT NULL DEFAULT '0' COMMENT '促销活动ID',
+  `goods_type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '实物或虚拟商品标志 1实物商品 0 虚拟商品 2 F码商品',
+  `market_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '市场价',
+  `price` decimal(19,2) NOT NULL DEFAULT '0.00' COMMENT '商品原价格',
+  `promotion_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品促销价格',
+  `cost_price` decimal(19,2) NOT NULL DEFAULT '0.00' COMMENT '成本价',
+  `point_exchange_type` tinyint(3) NOT NULL DEFAULT '0' COMMENT '积分兑换类型 0 非积分兑换 1 只能积分兑换 ',
+  `point_exchange` int(11) NOT NULL DEFAULT '0' COMMENT '积分兑换',
+  `give_point` int(11) NOT NULL DEFAULT '0' COMMENT '购买商品赠送积分',
+  `is_member_discount` int(1) NOT NULL DEFAULT '0' COMMENT '参与会员折扣',
+  `shipping_fee` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '运费 0为免运费',
+  `shipping_fee_id` int(11) NOT NULL DEFAULT '0' COMMENT '售卖区域id 物流模板id  ns_order_shipping_fee 表id',
+  `stock` int(10) NOT NULL DEFAULT '0' COMMENT '商品库存',
+  `max_buy` int(11) NOT NULL DEFAULT '0' COMMENT '限购 0 不限购',
+  `clicks` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '商品点击数量',
+  `min_stock_alarm` int(11) NOT NULL DEFAULT '0' COMMENT '库存预警值',
+  `sales` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '销售数量',
+  `collects` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '收藏数量',
+  `star` tinyint(3) unsigned NOT NULL DEFAULT '5' COMMENT '好评星级',
+  `evaluates` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评价数',
+  `shares` int(11) NOT NULL DEFAULT '0' COMMENT '分享数',
+  `province_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '一级地区id',
+  `city_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '二级地区id',
+  `picture` int(11) NOT NULL DEFAULT '0' COMMENT '商品主图',
+  `keywords` varchar(255) NOT NULL DEFAULT '' COMMENT '商品关键词',
+  `introduction` varchar(255) NOT NULL DEFAULT '' COMMENT '商品简介，促销语',
+  `description` text NOT NULL COMMENT '商品详情',
+  `QRcode` varchar(255) NOT NULL DEFAULT '' COMMENT '商品二维码',
+  `code` varchar(50) NOT NULL DEFAULT '' COMMENT '商家编号',
+  `is_stock_visible` int(1) NOT NULL DEFAULT '0' COMMENT '页面不显示库存',
+  `is_hot` int(1) NOT NULL DEFAULT '0' COMMENT '是否热销商品',
+  `is_recommend` int(1) NOT NULL DEFAULT '0' COMMENT '是否推荐',
+  `is_new` int(1) NOT NULL DEFAULT '0' COMMENT '是否新品',
+  `is_pre_sale` int(11) DEFAULT '0',
+  `is_bill` int(1) NOT NULL DEFAULT '0' COMMENT '是否开具增值税发票 1是，0否',
+  `state` tinyint(3) NOT NULL DEFAULT '1' COMMENT '商品状态 0下架，1正常，10违规（禁售）',
+  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
+  `img_id_array` varchar(1000) DEFAULT NULL COMMENT '商品图片序列',
+  `sku_img_array` varchar(1000) DEFAULT NULL COMMENT '商品sku应用图片列表  属性,属性值，图片ID',
+  `match_point` float(10,2) DEFAULT NULL COMMENT '实物与描述相符（根据评价计算）',
+  `match_ratio` float(10,2) DEFAULT NULL COMMENT '实物与描述相符（根据评价计算）百分比',
+  `real_sales` int(10) NOT NULL DEFAULT '0' COMMENT '实际销量',
+  `goods_attribute_id` int(11) NOT NULL DEFAULT '0' COMMENT '商品类型',
+  `goods_spec_format` text NOT NULL COMMENT '商品规格',
+  `goods_weight` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '商品重量',
+  `goods_volume` decimal(8,2) NOT NULL DEFAULT '0.00' COMMENT '商品体积',
+  `shipping_fee_type` int(11) NOT NULL DEFAULT '1' COMMENT '计价方式1.重量2.体积3.计件',
+  `extend_category_id` varchar(255) DEFAULT NULL,
+  `extend_category_id_1` varchar(255) DEFAULT NULL,
+  `extend_category_id_2` varchar(255) DEFAULT NULL,
+  `extend_category_id_3` varchar(255) DEFAULT NULL,
+  `supplier_id` int(11) NOT NULL DEFAULT '0' COMMENT '供货商id',
+  `sale_date` int(11) DEFAULT '0' COMMENT '上下架时间',
+  `create_time` int(11) DEFAULT '0' COMMENT '商品添加时间',
+  `update_time` int(11) DEFAULT '0' COMMENT '商品编辑时间',
+  `min_buy` int(11) NOT NULL DEFAULT '0' COMMENT '最少买几件',
+  `virtual_goods_type_id` int(11) DEFAULT '0' COMMENT '虚拟商品类型id',
+  `production_date` int(11) NOT NULL DEFAULT '0' COMMENT '生产日期',
+  `shelf_life` varchar(50) NOT NULL DEFAULT '' COMMENT '保质期',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `url` varchar(200) NOT NULL,
+  `pid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+INSERT INTO `sys_permission` VALUES ('1', 'ROLE_HOME', 'home', '/', null);
+INSERT INTO `sys_permission` VALUES ('2', 'ROLE_ADMIN', 'ABel', '/admin', null);
+
+-- ----------------------------
+-- Table structure for sys_permission_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission_role`;
+CREATE TABLE `sys_permission_role` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) unsigned NOT NULL,
+  `permission_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_permission_role
+-- ----------------------------
+INSERT INTO `sys_permission_role` VALUES ('1', '1', '1');
+INSERT INTO `sys_permission_role` VALUES ('2', '1', '2');
+INSERT INTO `sys_permission_role` VALUES ('3', '2', '1');
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO `sys_role` VALUES ('2', 'ROLE_USER');
+
+-- ----------------------------
+-- Table structure for sys_role_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_user`;
+CREATE TABLE `sys_role_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sys_user_id` bigint(20) unsigned NOT NULL,
+  `sys_role_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_role_user
+-- ----------------------------
+INSERT INTO `sys_role_user` VALUES ('1', '1', '1');
+INSERT INTO `sys_role_user` VALUES ('2', '2', '2');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'admin', 'admin');
+INSERT INTO `sys_user` VALUES ('2', 'abel', 'abel');
+
+-- ----------------------------
+-- Table structure for tb_account
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_account`;
+CREATE TABLE `tb_account` (
+  `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_pwd` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '密码',
+  `address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '地址',
+  `last_login_date` datetime NOT NULL COMMENT '最后登录日期',
+  `last_login_ip` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '最后登录ip',
+  `login_account` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '登录账户',
+  `login_count` int(10) NOT NULL COMMENT '登录次数',
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '会员姓名',
+  `reg_time` datetime NOT NULL COMMENT '注册时间',
+  `status` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '状态：active ,inactive ,locked,',
+  `tel` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '电话号码',
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tb_account
+-- ----------------------------
